7ac00355d28c32cfebc895245f0da3ae08aefa65
Rahul Shah <rahulsanjay18@gmail.com>
2020-05-22 23:59:42 -0400

    finished meshes, starting shaders

---
+++ Shaders/Assets/Scripts/RotationTransformation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTransformation : Transformation
+{
+    public Vector3 rotation;
+	
+	public override Vector3	Apply(Vector3 point){
+		float radX = rotation.x * Mathf.Deg2Rad;
+		float radY = rotation.y * Mathf.Deg2Rad;
+		float radZ = rotation.z * Mathf.Deg2Rad;
+		float sinX = Mathf.Sin(radX);
+		float cosX = Mathf.Cos(radX);
+		float sinY = Mathf.Sin(radY);
+		float cosY = Mathf.Cos(radY);
+		float sinZ = Mathf.Sin(radZ);
+		float cosZ = Mathf.Cos(radZ);
+
+		Vector3 xAxis = new Vector3(
+			cosY * cosZ,
+			cosX * sinZ + sinX * sinY * cosZ,
+			sinX * sinZ - cosX * sinY * cosZ
+		);
+		Vector3 yAxis = new Vector3(
+			-cosY * sinZ,
+			cosX * cosZ - sinX * sinY * sinZ,
+			sinX * cosZ + cosX * sinY * sinZ
+		);
+		Vector3 zAxis = new Vector3(
+			sinY,
+			-sinX * cosY,
+			cosX * cosY
+		);
+
+		return xAxis * point.x + yAxis * point.y + zAxis * point.z;
+	}
+}
---
+++ Shaders/Assets/Scripts/TransformationGrid.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformationGrid : MonoBehaviour
+{
+    public Transform prefab;
+	public int grid_resolution = 10;
+	
+	Transform[] grid;
+	List<Transformation> transformations;
+	
+	void Awake(){
+		grid = new Transform[grid_resolution * grid_resolution * grid_resolution];
+		for (int i = 0, z = 0; z < grid_resolution; z++){
+			for(int y = 0; y < grid_resolution; y++){
+				for(int x = 0; x < grid_resolution; x++, i++){
+					grid[i] = CreateGridPoint(x, y, z);
+				}
+			}
+		}
+		
+		transformations = new List<Transformation>();
+	}
+	
+	Transform CreateGridPoint(int x, int y, int z){
+		Transform point = Instantiate<Transform>(prefab);
+		point.localPosition = GetCoordinates(x, y, z);
+		point.GetComponent<MeshRenderer>().material.color = new Color(
+			(float)x / grid_resolution,
+			(float)y / grid_resolution,
+			(float)z / grid_resolution);
+		return point;
+	}
+	
+	Vector3 GetCoordinates(int x, int y, int z){
+		return new Vector3(
+			x - (grid_resolution - 1) * .5f,
+			y - (grid_resolution - 1) * .5f,
+			z - (grid_resolution - 1) * .5f);
+	}
+	
+	void Update(){
+		GetComponents<Transformation>(transformations);
+		for(int i = 0, z = 0; z < grid_resolution; z++){
+			for(int y = 0; y < grid_resolution; y++){
+				for(int x = 0; x < grid_resolution; x++, i++){
+					grid[i].localPosition = TransformPoint(x, y, z);
+				}
+			}
+		}
+	}
+	
+	Vector3 TransformPoint(int x, int y, int z){
+		Vector3 coordinates = GetCoordinates(x, y, z);
+		for(int i = 0; i < transformations.Count; i++){
+			coordinates = transformations[i].Apply(coordinates);
+		}
+		return coordinates;
+	}
+	
+	
+}
